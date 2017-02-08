e6a80fc1f5791c4609ec201ab7c06d6604bd5fd3
Denys Dovhan <email@denysdovhan.com>
2017-02-08 22:06:46 +0200

    Clean-up code

--- index.js
+++ index.js
@@ -7,34 +7,30 @@ const path = require('path');
 
 const resolveEnum = (choice, opts) => {
   const defaultValue = 1; // CommonJS/ES5/Preserve JSX defaults
-  if (!choice) {
-    return defaultValue;
-  }
-  if (!isNaN(choice)) {
-    return choice - 0;
-  }
+
+  if (!choice) return defaultValue;
+  if (!isNaN(choice)) return choice - 0;
+
   for (const opt of Object.keys(opts)) {
     if (choice && choice.toUpperCase() === opt.toUpperCase()) {
       return opts[opt];
     }
   }
+
   return defaultValue;
 };
 
-const getTsconfig = root => {
-  if (!root) {
-    return {};
-  }
-  const file = path.resolve(root, 'tsconfig.json');
-  let tsconf;
+const getTsconfig = configRoot => {
+  if (!configRoot) return {};
+
+  const file = path.resolve(configRoot, 'tsconfig.json');
+
   try {
-    tsconf = require(file);
+    return require(file).compilerOptions;
   } catch (e) {
     return {};
   }
-  return tsconf.compilerOptions || {};
 };
-
 
 const findLessOrEqual = (haystack, needle) => {
   let i = 0;
@@ -53,27 +49,35 @@ const toMeaningfulMessage = err => `Error ${err.code}: ${err.messageText} (${err
 
 class TypeScriptCompiler {
   constructor(config) {
-    if (!config) config = {};
-    const options = config.plugins &&
-      config.plugins.brunchTypescript || {};
+    if (!config) {
+      config = {};
+    }
+
+    const options = config.plugins && config.plugins.brunchTypescript || {};
     this.options = getTsconfig(config.paths && config.paths.root);
+
     Object.keys(options).forEach(key => {
       if (key === 'sourceMap' || key === 'ignore') return;
       this.options[key] = options[key];
     });
+
     this.options.module = resolveEnum(this.options.module, ts.ModuleKind);
     this.options.target = resolveEnum(this.options.target, ts.ScriptTarget);
     this.options.jsx = resolveEnum(this.options.jsx, ts.JsxEmit);
-    this.options.emitDecoratorMetadata = this.options.emitDecoratorMetadata !== false,
-    this.options.experimentalDecorators = this.options.experimentalDecorators !== false,
-    this.options.noEmitOnError = false; // This can't be true when compiling this way.
+    this.options.emitDecoratorMetadata = this.options.emitDecoratorMetadata !== false;
+    this.options.experimentalDecorators = this.options.experimentalDecorators !== false;
+    this.options.noEmitOnError = false; // This can"t be true when compiling this way.
+
     delete this.options.moduleResolution;
+
     this.options.sourceMap = !!config.sourceMaps;
-    this.isIgnored = anymatch(options.ignore || /^(bower_components|vendor|node_modules)/);
+    this.isIgnored = anymatch(options.ignore || /(^bower_components|vendor|node_modules)/);
+
     if (this.options.pattern) {
       this.pattern = this.options.pattern;
       delete this.options.pattern;
     }
+
     if (this.options.ignoreErrors) {
       if (this.options.ignoreErrors === true) {
         this.ignoreAllErrors = true;
@@ -85,9 +89,8 @@ class TypeScriptCompiler {
   }
 
   compile(params) {
-    if (this.isIgnored(params.path)) {
-      return Promise.resolve(params);
-    }
+    if (this.isIgnored(params.path)) return Promise.resolve(params);
+
     const tsOptions = {
       fileName: params.path,
       reportDiagnostics: true,
@@ -96,23 +99,25 @@ class TypeScriptCompiler {
 
     return new Promise((resolve, reject) => {
       let compiled;
+
       try {
         compiled = transpileModule(params.data, tsOptions);
         let reportable = compiled.diagnostics;
+
         if (this.ignoreAllErrors === true) {
           reportable = [];
         } else if (this.ignoreErrors) {
           reportable = reportable.filter(err => !this.ignoreErrors.has(err.code));
         }
+
         if (reportable.length) {
           reject(reportable.map(toMeaningfulMessage).join('\n'));
         }
       } catch (err) {
         return reject(err);
       }
-      const result = {data: compiled.outputText || compiled};
 
-      result.data += '\n';
+      const result = {data: `${compiled.outputText || compiled}\n`};
 
       if (compiled.sourceMapText) {
         // Fix the sources path so Brunch can merge them.
@@ -120,6 +125,7 @@ class TypeScriptCompiler {
         rawMap.sources[0] = params.path;
         result.map = JSON.stringify(rawMap);
       }
+
       resolve(result);
     });
   }
--- transpile.js
+++ transpile.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const ts = require('typescript');
 
 const ignoredErrors = new Set([
@@ -8,39 +9,49 @@ const ignoredErrors = new Set([
   2322, // Type '{0}' is not assignable to type '{1}'.
   2339, // Property '{0}' does not exist on type '{1}'.
 ]);
+
 const filterErrors = err => !ignoredErrors.has(err.code);
 
 const transpileModule = (input, transpileOptions) => {
-  var options = ts.clone(transpileOptions.compilerOptions);
+  const options = ts.clone(transpileOptions.compilerOptions);
 
   options.isolatedModules = true;
-    // transpileModule does not write anything to disk so there is no need to verify that there are no conflicts between input and output paths.
+  // transpileModule does not write anything to disk so there is no need to verify that there are no conflicts between input and output paths.
   options.suppressOutputPathCheck = true;
-    // Filename can be non-ts file.
+  // Filename can be non-ts file.
   options.allowNonTsExtensions = true;
-    // We are not returning a sourceFile for lib file when asked by the program,
-    // so pass --noLib to avoid reporting a file not found error.
+  // We are not returning a sourceFile for lib file when asked by the program,
+  // so pass --noLib to avoid reporting a file not found error.
   options.noLib = true;
-    // We are not doing a full typecheck, we are not resolving the whole context,
-    // so pass --noResolve to avoid reporting missing file errors.
+  // We are not doing a full typecheck, we are not resolving the whole context,
+  // so pass --noResolve to avoid reporting missing file errors.
   options.noResolve = true;
-    // We do want to emit here, so specifically enable it.
+  // We do want to emit here, so specifically enable it.
   options.noEmit = false;
-    // if jsx is specified then treat file as .tsx
-  var inputFileName = transpileOptions.fileName || (options.jsx ? 'module.tsx' : 'module.ts');
-  var sourceFile = ts.createSourceFile(inputFileName, input, options.target);
+  // if jsx is specified then treat file as .tsx
+  const inputFileName = transpileOptions.fileName ||
+    (options.jsx ? 'module.tsx' : 'module.ts');
+
+  const sourceFile = ts.createSourceFile(inputFileName, input, options.target);
+
   if (transpileOptions.moduleName) {
     sourceFile.moduleName = transpileOptions.moduleName;
   }
+
   sourceFile.renamedDependencies = transpileOptions.renamedDependencies;
-  var newLine = ts.getNewLineCharacter(options);
-    // Output
-  var outputText;
-  var sourceMapText;
-    // Create a compilerHost object to allow the compiler to read and write files
-  var compilerHost = {
+
+  const newLine = ts.getNewLineCharacter(options);
+
+  // Output
+  let outputText;
+  let sourceMapText;
+
+  // Create a compilerHost object to allow the compiler to read and write files
+  const compilerHost = {
     getSourceFile(fileName /* target*/) {
-      return fileName === ts.normalizeSlashes(inputFileName) ? sourceFile : undefined;
+      return fileName === ts.normalizeSlashes(inputFileName) ?
+        sourceFile :
+        undefined;
     },
     writeFile(name, text /* writeByteOrderMark */) {
       if (ts.fileExtensionIs(name, '.map')) {
@@ -76,17 +87,21 @@ const transpileModule = (input, transpileOptions) => {
       return true;
     },
   };
-  var program = ts.createProgram([inputFileName], options, compilerHost);
-  var diagnostics;
+
+  const program = ts.createProgram([inputFileName], options, compilerHost);
+
+  let diagnostics;
   if (transpileOptions.reportDiagnostics) {
     diagnostics = [];
-    ts.addRange(/* to*/ diagnostics, /* from*/ program.getSyntacticDiagnostics(sourceFile));
-    ts.addRange(/* to*/ diagnostics, /* from*/ program.getSemanticDiagnostics(sourceFile).filter(filterErrors));
-    ts.addRange(/* to*/ diagnostics, /* from*/ program.getOptionsDiagnostics());
+    ts.addRange(/* to */ diagnostics, /* from */ program.getSyntacticDiagnostics(sourceFile));
+    ts.addRange(/* to */ diagnostics, /* from */ program.getSemanticDiagnostics(sourceFile).filter(filterErrors));
+    ts.addRange(/* to */ diagnostics, /* from */ program.getOptionsDiagnostics());
   }
-    // Emit
+
+  // Emit
   program.emit();
   ts.Debug.assert(outputText !== undefined, 'Output generation failed');
+
   return {outputText, diagnostics, sourceMapText};
 };
 
